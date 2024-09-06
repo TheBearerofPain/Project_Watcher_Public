80cc1a478efb3372cb44add47526289437dd07d6
CalvinKork <calvin.kork@zemind.ca>
2024-09-06 13:57:20 -0500

    Initial Setup for Steam OSS

--- Source/Project_Watcher/Project_Watcher.Build.cs
+++ Source/Project_Watcher/Project_Watcher.Build.cs
@@ -9,5 +9,6 @@ public class Project_Watcher : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+    }
 }
